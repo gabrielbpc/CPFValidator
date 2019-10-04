9b489de7abe745d9ce141527d706734f27140c90
Gabriel Silva <gsilva2510@hotmail.com>
2019-10-04 02:27:14 -0300

    adding test

--- Python/test_validator.py
+++ Python/test_validator.py
@@ -1,4 +1,7 @@
 from validator import Validator
 
-def test_sShouldReturnTrueWhenValidatingThisCPF():
-    assert Validator().validate("36812594881") == True
+def test_ShouldReturnFalseWhenValidatingThisCPF():
+    assert Validator().validate("11122233381") == False
+
+def test_ShouldReturnTrueWhenValidatingThisCPF():
+    assert Validator().validate("33880753016") == True
